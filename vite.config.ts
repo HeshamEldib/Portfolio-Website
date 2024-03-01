e241948e5a6db934cc05a8ba5254d131c6824a19
Hesham Eldib <heshamEldib27@gmail.com>
2024-03-01 20:23:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://heshameldib.github.io/Portfolio-Website/",
+  base: "/Portfolio-Website/",
   plugins: [react()],
 })
