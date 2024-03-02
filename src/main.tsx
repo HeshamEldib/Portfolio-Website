934eb58f43e222f791100d14f51c1662a95c09d0
HeshamEldib <heshameldib27@gmail.com>
2024-03-02 21:57:03 +0200

    finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import App from "./App.js";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
